257e7eb4790255a3feee0bfe895109a05884c2f6
kime3259 <152557625+kime3259@users.noreply.github.com>
2025-11-04 06:01:30 +0000

    edit

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-//css, disable or enable hot/cold, disallow same guess
+//css, disable or enable hot/cold, disallow same guess, optional guess limit, guess history
 let level, answer, score;
 const levelArr = document.getElementsByName("level");
 const scoreArr = [];
 date.textContent = time();
 playBtn.addEventListener("click", play);
 guessBtn.addEventListener("click", makeGuess);
+setInterval(time, 1000);
+time();
 function play(){
+    let named = document.getElementById("named").value.toLowerCase();
+    let name = named.charAt(0).toUpperCase() + named.slice(1);
+    if(name == ""){
+        msg.textContent = "Please type in a name";
+        return;
+    }
     score = 0;
     playBtn.disabled = true;
     guessBtn.disabled = false;
@@ -16,29 +24,35 @@ function play(){
         }
         levelArr[i].disabled = true;
     }
-    msg.textContent = "Guess a number from 1 to " + level;
+    msg.textContent = name + ", guess a number from 1 to " + level;
     answer = Math.floor(Math.random()*level) + 1;
     guess.placeholder = answer;
 }
 function makeGuess(){
+    let named = document.getElementById("named").value.toLowerCase();
+    let name = named.charAt(0).toUpperCase() + named.slice(1);
+    if(name == ""){
+        msg.textContent = "Please type in a name";
+        return;
+    }
     let userGuess = parseInt(guess.value);
     if(isNaN(userGuess) || userGuess < 1 || userGuess > level){
-        msg.textContent = "Enter a VALID number from 1 to " + level;
+        msg.textContent = name + ", enter a VALID number from 1 to " + level;
         return;
     }
     score++;
     if(userGuess > answer){
-        msg.textContent = "Too high, try again.";
+        msg.textContent = name + " has guessed too high, try again.";
     }
     else if(userGuess < answer){
-        msg.textContent = "Too low, try again.";
+        msg.textContent = name + " has guessed too low, try again.";
     }
     else{
         if(score != 1){
-            msg.textContent = "Correct! You guessed the number in " + score + " tries! Press play to play again.";
+            msg.textContent = "Correct! " + name + " guessed the number in " + score + " tries! Press play to play again.";
         }
         else{
-            msg.textContent = "Correct! You guessed the number in 1 try! Press play to play again.";
+            msg.textContent = "Correct! " + name + " guessed the number in 1 try! Press play to play again.";
         }
         updateScore();
         reset();
@@ -71,6 +85,38 @@ function updateScore(){
 }
 function time(){
     let d = new Date();
-//    d = d.getFullYear() + " " + d.getTime();
-    return d;
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let month = months[d.getMonth()];
+    let day = d.getDate();
+    if(day == 1 || day == 11 || day == 21|| day == 31){
+        day += "st";
+    }
+    else if(day == 2 || day == 12 || day == 22){
+        day += "nd";
+    }
+    else if(day == 3 || day == 13 || day == 23){
+        day += "rd";
+    }
+    else{
+        day += "th";
+    }
+    let year = d.getFullYear();
+    let hour = d.getHours();
+    let minute = d.getMinutes();
+    let second = d.getSeconds();
+    let ampm = "";
+    if(hour >= 12){
+        ampm = " PM";
+    }
+    else{
+        ampm += " AM";
+    }
+    if(minute < 10){
+        minute = "0" + minute;
+    }
+    if(second < 10){
+        second = "0" + second;
+    }
+    const nowTime = month + " " + day + ", " + year + ", " + hour % 12 + ":" + minute + ":" + second + ampm;
+    date.textContent = nowTime;
 }
